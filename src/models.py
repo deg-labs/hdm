99a88d6f47dabcab915e2a3fb26c6889f0829713
haturatu <taro@eyes4you.org>
2026-03-11 04:47:56 +0900

    feat: add liquidation notification

--- src/models.py
+++ src/models.py
@@ -4,6 +4,7 @@ from typing import Literal, Optional
 
 TradeType = Literal["FILL", "ORDER_PLACED", "ORDER_CANCELLED"]
 TradeSide = Literal["BUY", "SELL"]
+LiquidationKind = Literal["Full Liq.", "Partial Liq."]
 
 
 @dataclass
@@ -22,3 +23,5 @@ class Trade:
     start_position: Optional[float] = None
     closed_pnl: Optional[float] = None
     order_id: Optional[int] = None
+    is_liquidation: bool = False
+    liquidation_kind: Optional[LiquidationKind] = None
